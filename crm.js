667969faf9557ab3b21591f475b9448cf995c7f7
Ivankorytnik <koritnik2@gmail.com>
2026-09-22 15:05:47 +0300

    Fix CRM JavaScript syntax after Alfa upload handler

--- crm.js
+++ crm.js
@@ -170,4 +170,4 @@ $('#dynFrom').value='2026-09-01';$('#dynTo').value='2026-12-31';$('#navAnalytics
   }finally{
     alfaFile.disabled=false;
   }
-}sb?.auth.onAuthStateChange(async(ev,s)=>{if((ev==='SIGNED_IN'||ev==='TOKEN_REFRESHED')&&s)await open(s)});boot()})();
+});sb?.auth.onAuthStateChange(async(ev,s)=>{if((ev==='SIGNED_IN'||ev==='TOKEN_REFRESHED')&&s)await open(s)});boot()})();
